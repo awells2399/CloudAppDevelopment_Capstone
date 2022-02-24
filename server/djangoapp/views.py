99d2133a1f9895e174297c4cc1ce78e6dcf39a07
awells2399m <alwells239@gmail.com>
2022-02-24 12:38:08 -0500

    user sign in out

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,26 +15,53 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
+def get_home(request):
+    context={}
+    if request.method == "GET":
+        return render(request, 'djangoapp/index.html', context)
 
 
 # Create an `about` view to render a static about page
-# def about(request):
+def about(request):
+    context={}
+    if request.method=='GET':
+        return render(request,'djangoapp/about.html', context)
 # ...
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
-
+def contact(request):
+    context={}
+    if request.method=='GET':
+        return render(request,'djangoapp/contact.html', context)
+# ...
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/index.html', context)
+    else:
+        return render(request,'djangoapp/index.html', context)
 # ...
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 # ...
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
+def registration_request(request):
+    context={}
+    return render(request,'djangoapp/registration.html',context)
 # ...
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
